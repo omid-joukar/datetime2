58e89b9b294d000cc2c1db62e3a737a6f9476573
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-15 16:14:46 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Calendar;
+
+/**
+ * Created by KPS on 7/15/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        // Create a default calendar
+        Calendar cal = Calendar.getInstance();
+        // Get and display information of current date from the calendar:
+        System.out.println();
+        System.out.println("Year: " + cal.get(Calendar.YEAR));
+        System.out.println("Month: " + cal.get(Calendar.MONTH));
+        System.out.println("Day: " + cal.get(Calendar.DATE));
+        System.out.println("Hour: " + cal.get(Calendar.HOUR));
+        System.out.println("Minute: " + cal.get(Calendar.MINUTE));
+        System.out.println();
+    }
+}
